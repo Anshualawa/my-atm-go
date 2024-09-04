154d5474b35841df4eaa890f615bd17a27ac2a2b
Anshualawa <alawapappu@gmail.com>
2024-09-04 12:33:29 +0530

    create new branch and modify main go file

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 func main() {
 	accounts := make(map[string]*account.Account)
 	accounts["123456"] = &account.Account{AccountNumber: "123456", Pin: 1234, Balance: 999.0}
+	accounts["12345678"] = &account.Account{AccountNumber: "12345678", Pin: 4321, Balance: 500.0}
 
 	var accountNumber string
 	var pin int64
@@ -33,22 +34,22 @@ func main() {
 
 		switch option {
 		case 1:
-			fmt.Printf("Your balance is: %.2f\n", account.CheckBalance())
+			fmt.Printf("Your balance is: %.2f /-\n", account.CheckBalance())
 		case 2:
 			var depositAmount float64
 			fmt.Println("Enter deposit amount:")
 			fmt.Scan(&depositAmount)
 			AvilableBalance := account.Deposit(depositAmount)
-			fmt.Println("Deposit successful, Total Avilable Balance:", AvilableBalance)
+			fmt.Println("Deposit successful, Total Avilable Balance:", AvilableBalance, "/-")
 
 		case 3:
 			var withdrawAmount float64
 			fmt.Println("Enter withdrawal amount:")
 			fmt.Scan(&withdrawAmount)
 			if account.Withdraw(withdrawAmount) {
-				fmt.Println("Withdrawal successful")
+				fmt.Println("Withdrawal successful Total Avilable Balance:", account.CheckBalance(), "/-")
 			} else {
-				fmt.Println("Insufficient funds")
+				fmt.Println("Insufficient funds Total Avilable Balance:", account.CheckBalance(), "/-")
 			}
 		case 4:
 			fmt.Println("Exiting...")
